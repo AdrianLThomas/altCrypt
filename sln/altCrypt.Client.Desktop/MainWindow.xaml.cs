c9bee9992f8e18a9fc8f343bd877502809fa9ee8
Adrian L Thomas <adrian.thomas@gmail.com>
2015-09-06 19:23:55 +0100

    Added viewmodel. Removed drag and drop for folder selection for now.

--- sln/altCrypt.Client.Desktop/MainWindow.xaml.cs
+++ sln/altCrypt.Client.Desktop/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace altCrypt.Client.Desktop
 {
@@ -20,6 +8,12 @@ namespace altCrypt.Client.Desktop
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog {IsFolderPicker = true};
+            CommonFileDialogResult result = dialog.ShowDialog();
         }
     }
 }
