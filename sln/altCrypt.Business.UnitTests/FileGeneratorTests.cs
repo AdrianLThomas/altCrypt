72db2f9b5abe655b2c4b21d804c60af02bcea3cc
Adrian L Thomas <adrian.thomas@gmail.com>
2015-09-09 11:15:25 +0100

    Added business layer project (portable .net 4.6). Upgraded core lib to .net 4.6 portable.

---
+++ sln/altCrypt.Business.UnitTests/FileGeneratorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace altCrypt.Business.UnitTests
+{
+    [TestClass]
+    public class FileGeneratorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
