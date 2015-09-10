088eb1ee928d55075431d5b06c2359d8a3f652e1
Adrian L Thomas <adrian.thomas@gmail.com>
2015-09-10 23:44:49 +0100

    Fixed dependency issue intro'ed with refactor.

--- sln/altCrypt.Client.Desktop/ViewModel/ViewModelLocator.cs
+++ sln/altCrypt.Client.Desktop/ViewModel/ViewModelLocator.cs
@@ -24,7 +24,7 @@ namespace altCrypt.Client.Desktop.ViewModel
                 SimpleIoc.Default.Register<IKey>(() => new Key("Pass@w0rd1")); //TODO accept input from user
                 SimpleIoc.Default.Register<IIV, RandomIV>();
                 SimpleIoc.Default.Register<SymmetricAlgorithm, AesCryptoServiceProvider>();
-                SimpleIoc.Default.Register<IEncryptFiles, StreamEncryptor>();
+                SimpleIoc.Default.Register<IEncryptFiles, FileEncryptor>();
 
                 var encryptor = SimpleIoc.Default.GetInstance<IEncryptFiles>();
                 SimpleIoc.Default.Register<IFileProcessor>(() => new FileProcessor(fileExtension, encryptor));
