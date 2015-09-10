9349c62664609e3b4e2b8c41326a08e94dddb58b
Adrian L Thomas <adrian.thomas@gmail.com>
2015-09-10 20:27:55 +0100

    Revert "Revert "Split up StreamEncryptor in to FileEncryptor. Upgraded projects to .NET 4.6.""
    This reverts commit f8891bb3c96e6830ac3271899a5e0a9596ee5f84.

---
+++ sln/altCrypt.Core.x86/Encryption/FileEncryptor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using altCrypt.Core.Encryption;
+using altCrypt.Core.FileSystem;
+
+namespace altCrypt.Core.x86.Encryption
+{
+    public class FileEncryptor : StreamEncryptor, IEncryptFile, IEncryptFiles
+    {
+        public FileEncryptor(IKey key, IIV iv, SymmetricAlgorithm encryptionProvider)
+            : base(key, iv, encryptionProvider)
+        {
+        }
+
+        public void Encrypt(IFile<Stream> file)
+        {
+            if (file == null)
+                throw new ArgumentNullException(nameof(file));
+
+            Encrypt(new[] { file });
+        }
+
+        public void Encrypt(IEnumerable<IFile<Stream>> files)
+        {
+            if (files == null)
+                throw new ArgumentNullException(nameof(files));
+
+            foreach (var file in files)
+            {
+                string tempFilename = $"{file.FilePath}.temp";
+                using (var stream = new FileStream(tempFilename, FileMode.OpenOrCreate))
+                {
+                    this.EncryptToStream(file, stream);
+                    file.Write(stream);
+                }
+
+                File.Delete(tempFilename);
+            }
+        }
+
+        public void Decrypt(IFile<Stream> file)
+        {
+            if (file == null)
+                throw new ArgumentNullException(nameof(file));
+
+            Decrypt(new[] { file });
+        }
+
+        public void Decrypt(IEnumerable<IFile<Stream>> files)
+        {
+            if (files == null)
+                throw new ArgumentNullException(nameof(files));
+
+            foreach (var file in files)
+            {
+                string tempFilename = $"{file.FilePath}.temp";
+                using (var stream = new FileStream(tempFilename, FileMode.OpenOrCreate))
+                {
+                    this.DecryptToStream(file, stream);
+                    file.Write(stream);
+                }
+
+                File.Delete(tempFilename);
+            }
+        }
+    }
+}
